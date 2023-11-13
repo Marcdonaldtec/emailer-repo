36a806465cbd5bea3654b95461dae42ca042497c
Marcdonaldtec <95457625+Marcdonaldtec@users.noreply.github.com>
2023-11-13 14:14:17 -0500

    We just made some changes

--- my_emailer/setup.py
+++ my_emailer/setup.py
@@ -14,7 +14,7 @@ setup(
     description='A simple emailer library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/Marcdonaldtec/emailer-repo'
+    url='https://github.com/Marcdonaldtec/emailer-repo',
      classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
