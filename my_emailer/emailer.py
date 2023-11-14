6d13e5c3ec8472f1c9bb948768b7414af6ba50a4
Marcdonaldtec <95457625+Marcdonaldtec@users.noreply.github.com>
2023-11-14 01:42:06 -0500

    Update emailer.py

--- my_emailer/emailer.py
+++ my_emailer/emailer.py
@@ -1,18 +1,22 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from validate_email import validate_email
+from validate_email_address import validate_email
 
 def setup_email_server(server, port, sender_email, password):
     return {'server': server, 'port': port, 'sender_email': sender_email, 'password': password}
 
 def is_valid_email(email):
-    return validate_email(email, verify=True)
+    try:
+        result = validate_email(email, verify=False)
+        return result and '@' in email
+    except Exception as e:
+        print(f"Error validating email: {e}")
+    return False
 
 def send_email(recipient, subject, body, server=None):
     if server is None:
         raise ValueError("Sèvè imel la dwe konfigire avan ou voye yon imèl.")
-
 
     if not is_valid_email(recipient):
         raise ValueError("Imèl sa pa valide.")
@@ -34,6 +38,9 @@ def send_email(recipient, subject, body, server=None):
         server.login(sender_email, password)
         server.sendmail(sender_email, recipient, msg.as_string())
 
-server = setup_email_server(server="smtp.example.com", port=587, sender_email="your_email@example.com", password="votre_modpas_sekrè")
-send_email("destinatè@example.com", "Sijè Imel la", "Imel la", server=server)
+recipient_email = input("Entrez l'adresse email du destinataire : ")
+email_subject = input("Entrez le sujet de l'email : ")
+email_body = input("Entrez le contenu de l'email : ")
 
+server = setup_email_server(server="smtp.gmail.com", port=587, sender_email="marcdonaldtech40@gmail.com", password="msbx kmxx omit uthm ")
+send_email(recipient=recipient_email, subject=email_subject, body=email_body, server=server)
